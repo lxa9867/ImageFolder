9bf47db0937bcc163289947a226a4a175f2831c6
qiuk2 <kaiqiu@andrew.cmu.edu>
2025-03-20 20:47:25 -0400

    fix utils

--- utils/logger.py
+++ utils/logger.py
@@ -21,11 +21,29 @@ from accelerate.logging import MultiProcessAdapter
 import logging
 from termcolor import colored
 
+import torch.distributed as dist
 from iopath.common.file_io import PathManager as PathManagerClass
 
 __all__ = ["setup_logger", "PathManager"]
 
 PathManager = PathManagerClass()
+
+def create_logger(logging_dir):
+    """
+    Create a logger that writes to a log file and stdout.
+    """
+    if dist.get_rank() == 0:  # real logger
+        logging.basicConfig(
+            level=logging.INFO,
+            format='[\033[34m%(asctime)s\033[0m] %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            handlers=[logging.StreamHandler(), logging.FileHandler(f"{logging_dir}/log.txt")]
+        )
+        logger = logging.getLogger(__name__)
+    else:  # dummy logger (does nothing)
+        logger = logging.getLogger(__name__)
+        logger.addHandler(logging.NullHandler())
+    return logger
 
 
 class _ColorfulFormatter(logging.Formatter):
